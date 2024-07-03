79386033e1d22dbea2473b038a68a086cd1b69c7
Christoooophe <christophealfier02@gmail.com>
2024-07-03 16:10:02 +0200

    Ajout question suivante

--- renderer.js
+++ renderer.js
@@ -1,27 +1,37 @@
 const buttonSend = document.getElementById("button-send");
-let reponse = "";
 let questions = "";
-
+let response = "";
+let increment = 0;
 const game = document.getElementById("game");
 buttonSend.addEventListener('click', () => {
     window.electronAPI.getQuestions();
     window.electronAPI.getLoadedQuestions(getQuestions);
-    getQuestionRenderer(questions[0]);
+    getQuestionRenderer(questions[increment]);
 })
 
 function getQuestionRenderer(data) {
-    const balise = document.getElementById("bloc-question")
+    response = data.reponse;
+    const balise = document.getElementById("bloc-question");
     if (balise) {
         game.removeChild(balise);
     }
     const divQuestion = document.createElement('div')
     const mappedQuestions = data.choices.map((item) => {
-        return `<button className="possible-answer">${item}</button>`;
+        return `<button onclick="getResponse(this.textContent, response)" class="possible-answer">${item}</button>`;
     });
     divQuestion.id = "bloc-question";
     divQuestion.innerHTML = `<p>${data.question}</p>`
     divQuestion.innerHTML += mappedQuestions;
     game.appendChild(divQuestion)
+}
+
+function getResponse(e, reponse) {
+    console.log(e, reponse)
+    increment++;
+    if (increment > questions.length - 1) {
+        return alert("GG !")
+    }
+    getQuestionRenderer(questions[increment])
 }
 
 function getQuestions(data) {
