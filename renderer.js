8813983880bc13f506a47c604d66d5e22adfb74d
Christoooophe <christophealfier02@gmail.com>
2024-07-03 15:11:41 +0200

    push pour test

--- renderer.js
+++ renderer.js
@@ -1,13 +1,27 @@
 const buttonSend = document.getElementById("button-send");
 let reponse = "";
 let questions = "";
+
+const game = document.getElementById("game");
 buttonSend.addEventListener('click', () => {
     window.electronAPI.getQuestions();
     window.electronAPI.getLoadedQuestions(getQuestions);
+    getQuestionRenderer(questions[0]);
+    console.log("heeeeey ", questions[0]);
 })
 
+function getQuestionRenderer(data) {
+    const balise = document.getElementById("bloc-question")
+    if (balise) {
+        game.removeChild(balise);
+    }
+    const divQuestion = document.createElement('p')
+    divQuestion.id = "bloc-question"
+    console.log(data)
+    divQuestion.textContent = data.question
+    game.appendChild(divQuestion)
+}
 
 function getQuestions(data) {
     questions = data;
-    console.log(questions)
 }
